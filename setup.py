ba5b87294a27d83a54a41661dd9977fd32ff1653
Chandan Kumar <ck22abf@herts.ac.uk>
2024-07-03 23:17:12 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'chan4kum',
+    author_email= 'chandankumarsingh1351@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
